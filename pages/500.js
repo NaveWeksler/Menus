5b13c97a55129830acdb78f1e4d9720a59f9d091
Oferlevy <06ofer@gmail.com>
2022-09-21 14:06:24 +0300

    fixed 500.js

--- pages/500.js
+++ pages/500.js
@@ -1,18 +1,3 @@
-import style from "styles/errorPage.module.css";
-
-const ServerError = () => (
-    <div className={`${style.marginText} fullscreen d-flex justify-content-center align-items-center`}>
-        <div className="w-25">
-            <h2><strong>500</strong> Internal Server Error</h2>
-            
-        </div>
-        <hr className="border-2 w-25"/>
-        <span className="lead">
-            Sorry, There is an Error in The Server. Please Try Again Later. 
-            <Link href="/"><a className="">Go To Main Menu</a></Link>
-        </span>
-    </div>
-);
+const ServerError = () => <div>Server Error</div>;
 
 export default ServerError;
-
