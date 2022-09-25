64b7ec10deb17a26ec944225f7bd96767cb189ea
Nave Weksler <naveweksler@gmail.com>
2022-09-25 15:24:41 +0300

    added login, fixed auth, fixed user model

---
+++ pages/api/auth/login/accountLogin.js
@@ -0,0 +1,40 @@
+import { rest, withDB } from "lib/util/middleware";
+import User from "lib/models/user";
+import { getCSRFToken, getSessionToken, getSessionExpireMs } from "lib/tokenHandler";
+const debug = require("debug")("menus:accountLogin");
+
+const checkUser = (username, password) => {
+    return /^[a-zA-Z0-9]{3,10}$/.test(username) && /^[a-zA-Z0-9]{5,18}$/.test(password);
+}
+
+const handler = rest.post(withDB(async (req, res) => {
+    debug("BODY: %O", req.body);
+    const {username, password} = req.body;
+
+    if (!username || !password || !checkUser(username, password)) {
+        return res.status(400).end();
+    }
+
+
+    const sessionToken = getSessionToken(), sessionTokenExpMs = getSessionExpireMs();
+    const csrfToken = getCSRFToken();
+
+    debug("query for account and set session");
+    const status = await User.updateOne(
+        {username: username, password: password},
+         {sessionToken, sessionTokenExpMs}).exec();
+         
+    debug("account found status: %O", status);
+    if (status.matchedCount !== 1) {
+        return res.status(401).end();
+    }
+
+    res.setHeader("set-cookie", [
+        "sessionToken=" + sessionToken + "; path=/;  samesite=strict; httponly; secure;",
+         "csrfToken=" + csrfToken + "; path=/;  samesite=strict; httponly; secure;"]);
+
+    res.status(200).json({csrfToken});
+
+}));
+
+export default handler;
