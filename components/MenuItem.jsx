bc5f98219d04572cfc1185bd7050f9e8d4c8c6f2
Oferlevy <06ofer@gmail.com>
2022-09-29 15:55:07 +0300

    Revert "switched to tailwind"
    This reverts commit b60ed2975c85b45bd1cbbad25b13012a84e23a5b.

--- components/MenuItem.jsx
+++ components/MenuItem.jsx
@@ -1,24 +1,27 @@
-import Image from 'next/image';
+import styles from '../styles/MenuItem.module.css';
 
-const MenuItem = ({ name, description, price, image }) => {
-    // NOTE: MenuItem is designed for hebrew
-    return (
-        <div className='flex flex-row-reverse justify-between max-w-screen-sm px-4 py-5 border border-gray-100 dark:border-gray-700 rounded sm:mx-2 hover:bg-gray-50 dark:hover:bg-gray-900 transition'>
-            <div className='text-right'>
-                <p className='font-medium pb-1'>{name}</p>
-                <p className='text-xs sm:text-sm text-gray-500 dark:text-gray-400'>
-                    {description}
-                </p>
-                <p className='text-blue-400 text-sm font-light pt-1.5'>
-                    {price}
-                </p>
-            </div>
-
-            <div className='relative w-32 h-20 sm:w-44 sm:h-28 rounded-lg overflow-hidden transition-height duration-300'>
-                <Image src={image} alt={name} layout='fill' objectFit='cover' />
-            </div>
+const MenuItem = ({ name, description, price, imageURL }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.pictureContainer}>
+        <img
+          className={styles.picture}
+          draggable={false}
+          src={imageURL}
+          alt='burger'
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.horizontalContainer}>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.price}>{price}</div>
         </div>
-    );
+        {description && (
+          <div className={styles.description}>{data.description}</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default MenuItem;
--- pages/404.jsx
+++ pages/404.jsx
@@ -1,19 +1,22 @@
+import style from 'styles/errorPage.module.css';
 import Link from 'next/link';
 
 const PageNotFound = () => (
-    <div className='h-screen p-2 pb-[30%] flex flex-col justify-center items-center'>
-        <h2 className='text-3xl font-medium'>
-            <strong>404</strong> Not Found
-        </h2>
+  <div
+    className={`${style.marginText} fullscreen d-flex justify-content-center align-items-center`}
+  >
+    <h2 className=''>
+      <strong>404</strong> Not Found
+    </h2>
 
-        <hr className='border-r border-gray-400 w-80 max-w-[90%] mt-6 mb-4' />
-        <span className='text-xl font-light'>
-            Sorry, This Page Does Not Exsist.{' '}
-            <Link href='/'>
-                <a className='text-blue-500'>Go To Main Menu</a>
-            </Link>
-        </span>
-    </div>
+    <hr className='border-2 w-25' />
+    <span className='lead'>
+      Sorry, This Page Does Not Exsist.{' '}
+      <Link href='/'>
+        <a className=''>Go To Main Menu</a>
+      </Link>
+    </span>
+  </div>
 );
 
 export default PageNotFound;
