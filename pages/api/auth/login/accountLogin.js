9431261c8d056a141ffaf24b0b227a4c0ec075d6
Nave Weksler <naveweksler@gmail.com>
2022-10-03 19:45:51 +0300

    hashed, salt password to store in db login, signup

--- pages/api/auth/login/accountLogin.js
+++ pages/api/auth/login/accountLogin.js
@@ -1,37 +1,54 @@
-import { rest, withDB } from "lib/util/middleware";
-import User from "lib/models/user";
-import { getCSRFToken, getSessionToken, getSessionExpireMs } from "lib/tokenHandler";
-import {checkUser} from "lib/userValidator";
-const debug = require("debug")("menus:accountLogin");
+import { rest, withDB } from 'lib/util/middleware';
+import User from 'lib/models/user';
+import {
+    getCSRFToken,
+    getSessionToken,
+    getSessionExpireMs,
+} from 'lib/tokenHandler';
+import { checkUser } from 'lib/userValidator';
+import { comparePsw } from 'lib/pswStorage';
+const debug = require('debug')('menus:accountLogin');
 
-const handler = rest.post(withDB(async (req, res) => {
-    debug("BODY: %O", req.body);
-    const {username, password} = req.body;
+const handler = rest.post(
+    withDB(async (req, res) => {
+        debug('BODY: %O', req.body);
+        const { username, password } = req.body;
 
-    if (!username || !password || !checkUser(username, password)) {
-        return res.status(400).end();
-    }
+        if (!username || !password || !checkUser(username, password)) {
+            return res.status(400).end();
+        }
 
+        debug('query for account');
+        const userToAuth = await User.findOne({ username })
+            .select('password salt ')
+            .lean();
+        debug('account found. compare with request');
+        if (!comparePsw(password, userToAuth.password, userToAuth.salt)) {
+            return res.status(401).end();
+        }
+        debug('valid request, update session');
 
-    const sessionToken = getSessionToken(), sessionTokenExpMs = getSessionExpireMs();
-    const csrfToken = getCSRFToken();
+        const sessionToken = getSessionToken(),
+            sessionTokenExpMs = getSessionExpireMs(),
+            csrfToken = getCSRFToken();
 
-    debug("query for account and set session");
-    const status = await User.updateOne(
-        {username: username, password: password},
-         {sessionToken, sessionTokenExpMs}).exec();
-         
-    debug("account found status: %O", status);
-    if (status.matchedCount !== 1) {
-        return res.status(401).end();
-    }
+        const status = await User.updateOne(
+            { _id: userToAuth._id },
+            { sessionToken, sessionTokenExpMs }
+        ).exec();
+        debug('updated status: %o', status);
 
-    res.setHeader("set-cookie", [
-        "sessionToken=" + sessionToken + "; path=/;  samesite=strict; httponly; secure;",
-         "csrfToken=" + csrfToken + "; path=/;  samesite=strict; httponly; secure;"]);
+        res.setHeader('set-cookie', [
+            'sessionToken=' +
+                sessionToken +
+                '; path=/;  samesite=strict; httponly; secure;',
+            'csrfToken=' +
+                csrfToken +
+                '; path=/;  samesite=strict; httponly; secure;',
+        ]);
 
-    res.status(200).json({csrfToken});
-
-}));
+        res.status(200).json({ csrfToken });
+    })
+);
 
 export default handler;
