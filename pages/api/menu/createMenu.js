c1c968b8a8b09419061195a89c7bcd2a64d937b7
Nave Weksler <naveweksler@gmail.com>
2022-10-05 12:46:46 +0300

    Revert "add menu api route. images are not working"
    This reverts commit 79d3e2bb84f2d6e614a20e94208d481f1aac9974.

--- pages/api/menu/createMenu.js
+++ pages/api/menu/createMenu.js
@@ -1,31 +1,7 @@
 import withAuth from 'lib/util/auth';
-import { toMenuItems } from 'lib/menuHandler';
-import { validateItems, validateTitle } from 'lib/menuValidator';
-import Menu from 'lib/models/menu';
-import { Types } from 'mongoose';
-const debug = require('debug')('menus:createMenu');
 
-const createMenu = withAuth(1, async (req, res) => {
-    const { title, items } = req.body;
-
-    if (!validateItems(items) || !validateTitle(title))
-        return res.status(400).end();
-
-    debug('valid data. generate items');
-
-    const menuId = new Types.ObjectId(); // generate menu id for refrencing by the items.
-    const ids = await toMenuItems(menuId, items);
-
-    debug('add menu');
-
-    const status = await Menu.updateOne(
-        { title },
-        { $setOnInsert: { _id: menuId, title, items: ids } },
-        { upsert: true }
-    ).exec();
-    debug('status: %o', status);
-
-    res.status(201).end();
+const createMenu = withAuth(1, (req, res) => {
+    const { title, items } = req.body; // title is [{name: string, price: string, image: }]
 });
 
 export default createMenu;
