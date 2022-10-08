5e0e73c9cd92feb9158e2d42e31bfca2909558c1
NirMannn <nirmann237@gmail.com>
2022-10-08 08:07:24 +0300

    updated design and made readble on light mode

--- components/MenuItem.jsx
+++ components/MenuItem.jsx
@@ -2,19 +2,24 @@ import Image from 'next/image';
 
 const MenuItem = ({ name, description, price, image }) => {
   return (
-    <div className='flex flex-row border rounded h-40 mx-5'>
-      <div className='w-[25vw] h-full relative overflow-hidden rounded-lg'>
+    <div className='flex flex-row border border-black dark:border-slate-100 rounded h-40 mx-2'>
+      <div className='w-[25vw] h-full relative overflow-hidden rounded-r-lg'>
         <Image src={image} alt={name} layout='fill' objectFit='cover' />
       </div>
       <div className='flex-1 flex flex-col items-center justify-start mx-2'>
-        <span className='font-light text-3xl text-center flex items-center justify-center '>
-          {name}
-        </span>
-        <span className='text-blue-400 px-2 flex items-center justify-center text-xl mt'>
-          {price}
-        </span>
+        <div className='w-full flex items-start justify-between '>
+          <div className=' dark:text-blue-400 px-2 flex items-center justify-start text-xl mt-2  '>
+            <span>{price}</span>
+          </div>
+          <span className='font-light text-3xl text-center flex items-center justify-center '>
+            {name}
+          </span>
+          <span className='text-transparent px-2 flex items-center justify-start text-xl mt'>
+            {price}
+          </span>
+        </div>
         <div className='flex w-full h-full justify-end mb-2'>
-          <span className='text-gray-300 font-thin flex items-end justify-end w-10/12 h-full text-xl'>
+          <span className='dark:text-gray-300 font-thin flex items-end justify-end w-full h-full text-xl'>
             {description}
           </span>
         </div>
