fbd8915ae91452eef22bfbb7e61532e61231d7d7
Nave Weksler <naveweksler@gmail.com>
2022-10-14 09:31:54 +0300

    reset password backend done

---
+++ pages/api/auth/resetPassword/changePassword.js
@@ -0,0 +1,37 @@
+import User from 'lib/models/user';
+import { storePsw } from 'lib/auth/pswStorage';
+import { checkUser } from 'lib/auth/userValidator';
+import { rest, withDB } from 'lib/util/middleware';
+import { randomBytes } from 'crypto';
+const debug = require('debug')('menus:changePassword');
+
+const handler = rest.post(
+    withDB(async (req, res) => {
+        const { token, email, newPassword } = req.body; // include email to prevent same token on two accounts
+        if (!token || !newPassword || !email || !checkUser(email, newPassword))
+            return res.status(400).end();
+
+        const salt = randomBytes(16).toString('base64');
+        const hashedPassword = await storePsw(newPassword, salt); // need to fix. storePsw might reject in which case an error will be thrown
+
+        debug('query to update password');
+        const status = await User.updateOne(
+            // update account if token is valid.
+            {
+                emailToken: token,
+                email,
+                emailTokenExpMs: { $gt: new Date().getTime() },
+            },
+            {
+                $set: { password: hashedPassword, salt },
+                $unset: { emailToken: '', emailTokenExpMs: '' }, // remove uncessery fields
+            }
+        ).exec();
+        debug('status: %o', status);
+
+        if (status.modifiedCount != 1) return res.status(401).end();
+        res.redirect(200, 'auth/login');
+    })
+);
+
+export default handler;
---
+++ pages/api/auth/resetPassword/requestReset.js
@@ -0,0 +1,48 @@
+import { rest, withDB } from 'lib/util/middleware';
+import sendHTML from 'lib/util/emailHandler';
+import { randomBytes } from 'crypto';
+import User from 'lib/models/user';
+const debug = require('debug')('menus:requestReset');
+
+// reset password. send email with token to reset password
+
+const handler = rest.post(
+    withDB(async (req, res) => {
+        const email = req.body.email;
+        if (!email) return res.status(400).end();
+
+        const token = randomBytes(3).toString('hex');
+
+        sendHTML(
+            'Reset Your Menus Password',
+            `
+        <h1>Reset Password</h1>
+        <p>
+            Enter this token to change your password.
+            <strong>
+                ${token.split('').join(' ')}
+            </strong>
+        </p>
+        <p><strong>If you did not request a password reset you dont need to do anything</strong></p>
+    `,
+            email
+        );
+        debug('new request, set token in db');
+        const status = await User.updateOne(
+            { email, state: { $ne: 2 } },
+            {
+                $set: {
+                    emailToken: token,
+                    emailTokenExpMs:
+                        new Date().getTime() +
+                        (parseInt(process.env.RESET_PSW_EXP_MS) | 0),
+                },
+            }
+        ).exec();
+        debug('status: %o', status);
+        if (status.modifiedCount != 1) return res.status(404).end();
+        res.status(200).end();
+    })
+);
+
+export default handler;
