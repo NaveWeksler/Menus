394c123ccb2e78142021babcd80fa7a3564b942b
Nave Weksler <naveweksler@gmail.com>
2022-10-17 10:26:03 +0300

    add SEO optimization

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,16 @@
+import { Html } from 'next/document';
 import Head from 'next/head';
 
 const Layout = ({ children }) => (
-    <>
+    <Html lang='en'>
         <Head>
             <title>Menus</title>
+            <meta name='description' content='Organize Small Sales' />
         </Head>
         <div className='h-screen'>
             <main className='w-full h-full'>{children}</main>
         </div>
-    </>
+    </Html>
 );
 
 export default Layout;
