66cffcd5c67988e4148e6ffe0548b3791fd473c0
Oferlevy <06ofer@gmail.com>
2022-11-12 07:57:41 +0200

    fixed bugs in 404, 500, no-orders

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,18 +1,19 @@
 import Link from 'next/link';
 
 const ServerError = () => (
-    <div className='h-screen p-2 pb-[30%] flex flex-col justify-center items-center overflow-x-hidden'>
+    <div className='h-screen p-2 pb-[30%] flex flex-col justify-center items-center'>
         <h2 className='text-3xl font-medium'>
             <strong>500</strong> Internal Server Error
         </h2>
 
-        <hr className='border-r border-gray-400 w-80 mt-6 mb-4' />
-        <span className='text-xl font-light'>
-            Sorry, There is an Error in The Server. Please Try Again Later.{' '}
-            <Link href='/'>
-                <a className='text-blue-500'>Go To Main Menu</a>
-            </Link>
-        </span>
+        <div className='max-w-[min(90%,50rem)] mt-4 pt-4 border-t border-gray-400'>
+            <span className='text-xl font-light'>
+                Sorry, There is an Error in The Server. Please Try Again Later.{' '}
+                <Link href='/'>
+                    <a className='text-blue-500'>Go To Main Menu</a>
+                </Link>
+            </span>
+        </div>
     </div>
 );
 
