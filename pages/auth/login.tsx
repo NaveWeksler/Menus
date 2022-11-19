e55f6c10143f200e760a664b27883fd247d616ef
Nave Weksler <naveweksler@gmail.com>
2022-11-19 21:11:54 +0200

    add sign  in with google button

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import fetcher from "lib/frontend/fetcher";
 import {useRouter} from "next/router";
+import Script from 'next/script';
 
 const Login = () => {
     const router = useRouter();
@@ -18,6 +19,9 @@ const Login = () => {
     }
 
     return (
+        <>
+        <Script src="https://accounts.google.com/gsi/client" async defer strategy='beforeInteractive'/>
+        
         <div className='flex justify-center items-center h-screen bg-gray-100 dark:bg-gray-700'>
             <div className='px-8 py-6 bg-white dark:bg-gray-800 rounded-lg shadow-lg dark:shadow-gray-600 text-right'>
                 <h2 className='text-xl text-center font-semibold'>כניסה</h2>
@@ -25,11 +29,28 @@ const Login = () => {
                     <p className='text-gray-600 text-sm dark:text-gray-300'>
                         :היכנס עם
                     </p>
-                    <Link href='#'>
-                        <a className='mx-2 p-1 bg-gray-200 rounded-full dark:bg-gray-600'>
-                            <FcGoogle size={20} />
-                        </a>
-                    </Link>
+
+                    <div id="g_id_onload"
+                        data-client_id={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+                        data-context="signin"
+                        data-ux_mode="popup"
+                        data-login_uri={process.env.NEXT_PUBLIC_GOOGLE_LOGIN_API}
+                        data-nonce=""
+                        data-auto_select="true"
+                        data-itp_support="true">
+                    </div>
+
+                    <div className="g_id_signin"
+                        data-type="standard"
+                        data-shape="rectangular"
+                        data-theme="outline"
+                        data-text="signin_with"
+                        data-size="large"
+                        data-logo_alignment="left">
+                    </div>
+
+                    
+
                 </div>
                 <div className='w-full height-8 border-b text-center my-4 leading-[0.1em] dark:border-gray-500'>
                     <span className='bg-white dark:bg-gray-800 px-2 text-gray-600 dark:text-gray-400'>
@@ -79,6 +100,7 @@ const Login = () => {
                 </form>
             </div>
         </div>
+        </>
     );
 };
 
